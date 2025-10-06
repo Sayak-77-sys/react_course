0737740f5d01ad76fd147f22de772be3b19720a3
Sayak-77-sys <s3151855@gmail.com>
2025-10-06 12:29:49 +0530

    initial setup

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+import Youtube from './Youtube';
+//import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>classic react app</h1>
+    <Youtube />
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Youtube.jsx
@@ -0,0 +1,7 @@
+
+
+function Youtube() {
+  return <h1>This is a youtube component</h1>;
+}
+
+export default Youtube;
